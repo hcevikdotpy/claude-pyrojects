ea45a1c21d63fd6272c63227627efd3602eddfbf
Huseyin <huseyin.cevik@green-air.info>
2024-08-18 23:02:11 +0200

    change package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='claude-pyrojects',
+    name='claude-pyrojects-cli',
     version='0.1.0',
     description='A tool for uploading and managing projects in Claude.ai',
     long_description=open('README.md').read(),
