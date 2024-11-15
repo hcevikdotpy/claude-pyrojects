1c09acc7729b8d1c0cf3781d23e3c7c77a5fc539
Huseyin Cevik <133138410+hcevikdotpy@users.noreply.github.com>
2024-11-15 01:13:31 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='claude-pyrojects',
-    version='0.1.1',
+    version='0.1.2',
     description='A tool for uploading and managing projects in Claude.ai',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
